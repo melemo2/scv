6230939378fc81660478c124227aaabefa5fcb09
melemo2 <sonmingi0123@gmail.com>
2021-11-10 14:07:48 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
+import * as getStockData from "./getStockData.js";
+import Chart from "./Chart.js";
 
 function App() {
+
+  var [record, r] = useState({d:[
+    ['2013/1/24', 2320.26,2320.26,2287.3,2362.94],
+    ['2013/1/25', 2300,2291.3,2288.26,2308.38],
+    ['2013/1/28', 2295.35,2346.5,2295.35,2346.92],
+    ['2013/1/29', 2347.22,2358.98,2337.35,2363.8],
+    ['2013/1/30', 2360.75,2382.48,2347.89,2383.76],
+    ['2013/1/31', 2383.43,2385.42,2371.23,2391.82],
+    ['2013/2/1', 2377.41,2419.02,2369.57,2421.15],
+    ['2013/2/4', 2425.92,2428.15,2417.58,2440.38],
+    ['2013/2/5', 2411,2433.13,2403.3,2437.42]]});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Chart data = {record} />
+      <button onClick={getStockData.getStock}>Activate Lasers</button>
     </div>
   );
 }
---
+++ src/Chart.js
@@ -0,0 +1,247 @@
+
+import React, {Component} from 'react';
+import ReactECharts from 'echarts-for-react';
+
+function splitData(data) {
+
+    var categoryData = [];
+    var values = []
+    for (var i = 0; i < data.length; i++) {
+        categoryData.push(data[i].splice(0, 1)[0]);
+        values.push(data[i])
+    }
+    return {
+        categoryData: categoryData,
+        values: values
+    };
+}
+
+function calculateMA(dayCount, data0) {
+    var result = [];
+    for (var i = 0, len = data0.values.length; i < len; i++) {
+        if (i < dayCount) {
+            result.push('-');
+            continue;
+        }
+        var sum = 0;
+        for (var j = 0; j < dayCount; j++) {
+            sum += data0.values[i - j][1];
+        }
+        result.push(sum / dayCount);
+    }
+    return result;
+}
+
+function getChart(StockRecord){
+
+    console.log(StockRecord);
+
+    var data0 = splitData(StockRecord);
+    var upColor = '#ec0000';
+    var upBorderColor = '#8A0000';
+    var downColor = '#00da3c';
+    var downBorderColor = '#008F28';
+
+    var option = {
+        title: {
+            text: 'charts',
+            left: 0
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'cross'
+            }
+        },
+        legend: {
+            data: ['日K', 'MA5', 'MA10', 'MA20', 'MA30']
+        },
+        grid: {
+            left: '10%',
+            right: '10%',
+            bottom: '15%'
+        },
+        xAxis: {
+            type: 'category',
+            data: data0.categoryData,
+            scale: true,
+            boundaryGap: false,
+            axisLine: {onZero: false},
+            splitLine: {show: false},
+            splitNumber: 20,
+            min: 'dataMin',
+            max: 'dataMax'
+        },
+        yAxis: {
+            scale: true,
+            splitArea: {
+                show: true
+            }
+        },
+        dataZoom: [
+            {
+                type: 'inside',
+                start: 50,
+                end: 100
+            },
+            {
+                show: true,
+                type: 'slider',
+                top: '90%',
+                start: 50,
+                end: 100
+            }
+        ],
+        series: [
+            {
+                name: '日K',
+                type: 'candlestick',
+                data: data0.values,
+                itemStyle: {
+                    color: upColor,
+                    color0: downColor,
+                    borderColor: upBorderColor,
+                    borderColor0: downBorderColor
+                },
+                markPoint: {
+                    label: {
+                        normal: {
+                            formatter: function (param) {
+                                return param != null ? Math.round(param.value) : '';
+                            }
+                        }
+                    },
+                    data: [
+                        {
+                            name: 'XX标点',
+                            coord: ['2013/5/31', 2300],
+                            value: 2300,
+                            itemStyle: {
+                                color: 'rgb(41,60,85)'
+                            }
+                        },
+                        {
+                            name: 'highest value',
+                            type: 'max',
+                            valueDim: 'highest'
+                        },
+                        {
+                            name: 'lowest value',
+                            type: 'min',
+                            valueDim: 'lowest'
+                        },
+                        {
+                            name: 'average value on close',
+                            type: 'average',
+                            valueDim: 'close'
+                        }
+                    ],
+                    tooltip: {
+                        formatter: function (param) {
+                            return param.name + '<br>' + (param.data.coord || '');
+                        }
+                    }
+                },
+                markLine: {
+                    symbol: ['none', 'none'],
+                    data: [
+                        [
+                            {
+                                name: 'from lowest to highest',
+                                type: 'min',
+                                valueDim: 'lowest',
+                                symbol: 'circle',
+                                symbolSize: 10,
+                                label: {
+                                    show: false
+                                },
+                                emphasis: {
+                                    label: {
+                                        show: false
+                                    }
+                                }
+                            },
+                            {
+                                type: 'max',
+                                valueDim: 'highest',
+                                symbol: 'circle',
+                                symbolSize: 10,
+                                label: {
+                                    show: false
+                                },
+                                emphasis: {
+                                    label: {
+                                        show: false
+                                    }
+                                }
+                            }
+                        ],
+                        {
+                            name: 'min line on close',
+                            type: 'min',
+                            valueDim: 'close'
+                        },
+                        {
+                            name: 'max line on close',
+                            type: 'max',
+                            valueDim: 'close'
+                        }
+                    ]
+                }
+            },
+            {
+                name: 'MA5',
+                type: 'line',
+                data: calculateMA(5, data0),
+                smooth: true,
+                lineStyle: {
+                    opacity: 0.5
+                }
+            },
+            {
+                name: 'MA10',
+                type: 'line',
+                data: calculateMA(10, data0),
+                smooth: true,
+                lineStyle: {
+                    opacity: 0.5
+                }
+            },
+            {
+                name: 'MA20',
+                type: 'line',
+                data: calculateMA(20, data0),
+                smooth: true,
+                lineStyle: {
+                    opacity: 0.5
+                }
+            },
+            {
+                name: 'MA30',
+                type: 'line',
+                data: calculateMA(30, data0),
+                smooth: true,
+                lineStyle: {
+                    opacity: 0.5
+                }
+            },
+    
+        ]
+    };
+    
+    return (
+        <ReactECharts option={option} />
+    );
+}
+
+class Chart extends React.Component{
+    render(){
+        return (
+            <div className="Chart">
+                {getChart(this.props.data.d)}
+            </div>   
+        );
+    }
+}
+
+export default Chart;
