9ceb86b479df196fdcbb5b9069b91f66cfdb2cf1
melemo2 <sonmingi0123@gmail.com>
2021-11-10 20:33:46 +0900

    [Update] chart & stockdata - fin

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,11 +1,10 @@
 
-import React, { useState, useEffect, Component } from 'react';
+import React, { useState, useEffect, Component, setState } from 'react';
 import ReactECharts from 'echarts-for-react';
-import getStock from "./getStockData.jsx";
 
 function getOption(Data){
+
     console.log(Data.X);
-    console.log(Data.Y);
     return {
         title : { text: '2013년 삼성전자 일봉' },
         tooltip : { trigger: 'axis',
@@ -27,18 +26,33 @@ function getOption(Data){
                 saveAsImage : {show: true}
             }
         },
-        dataZoom : { show : true, realtime: true, start : 50, end : 100 },
+        dataZoom : { show : true, realtime: true, start : 0, end : 100 },
         xAxis : [ {type : 'category', boundaryGap : true, axisTick: {onGap:false}, splitLine: {show:false}, data : Data.X} ],
         yAxis : [ {type : 'value', scale:true, boundaryGap: [0.01, 0.01] } ],
         series : [ {name:'일봉', type:'k', data: Data.Y} ]
     };                  
 }
 
+
 class Chart extends React.Component {
 
     constructor(props) { 
         super(props);
-        this.state = {data: getStock() , record: this.props.record};
+
+        this.state = {data: {} , record: this.props.record};
+
+        const axios = require('axios');
+  
+        axios.get('https://api.upbit.com/v1/candles/days?market=krw-btc&count=30')
+        .then(response => {
+          var Xlist = new Array();
+          var Ylist = new Array();
+          for(var i = 0; i < response.data.length; i++){
+            Xlist.push(response.data[i].candle_date_time_utc.split('T')[0]);
+            Ylist.push([response.data[i].trade_price, response.data[i].opening_price, response.data[i].low_price, response.data[i].high_price]);
+          }
+          this.setState( {data : {X : Xlist, Y : Ylist}});      
+        });   
     }
 
     render() {
--- src/getStockData.jsx
+++ src/getStockData.jsx
@@ -1,19 +1,19 @@
-import axios from 'axios';
 
-export default function getStock() {
-  
-  var Xlist = new Array();
-  var Ylist = new Array();
 
-  console.log("click");
+export default async function getStock() {
   const axios = require('axios');
-  axios.get('https://api.upbit.com/v1/candles/days?market=krw-btc&count=10')
+
+  axios.get('https://api.upbit.com/v1/candles/days?market=krw-btc&count=30')
   .then(response => {
+    var Xlist = new Array();
+    var Ylist = new Array();
     for(var i = 0; i < response.data.length; i++){
       Xlist.push(response.data[i].candle_date_time_utc.split('T')[0]);
       Ylist.push([response.data[i].trade_price, response.data[i].opening_price, response.data[i].low_price, response.data[i].high_price]);
     }
+
+    return {X : Xlist, Y : Ylist};
+    
   });
 
-  return {X : Xlist, Y : Ylist};
 }
